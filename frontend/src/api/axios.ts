ea09c22db0c6f0910f85e3ecdcaa14115e2ae074
Ayush Abhinandan <ayushabhinandan269@gmail.com>
2026-03-19 00:54:32 +0530

    updated api instead of local api

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: " https://gym-management-be-1.onrender.com/api",
 });
 
 // attach JWT token automatically
